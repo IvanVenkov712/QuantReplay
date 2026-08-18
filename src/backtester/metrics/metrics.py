81d4aab8c8a8f8d3c8fd9386234cff3174f4e24d
IvanVenkov712 <ivan.venkov.developer@gmail.com>
2026-08-18 15:19:35 +0300

    Added some metrics

--- src/backtester/metrics/metrics.py
+++ src/backtester/metrics/metrics.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 from collections.abc import Callable
+from math import sqrt
+from typing import List
 
 from typing_extensions import override, NamedTuple
 
@@ -57,3 +59,65 @@ class PerformanceAnalyzer:
             name: PerformanceAnalyzer.MetricData(metric.label, metric.calculate(results))
             for name, metric in self._metrics.items()
         }
+
+def total_return(results: BacktestResult) -> float:
+    vn = results.records[-1].portfolio_value_at_close
+    v1 = results.records[0].portfolio_value_at_close
+    return vn / v1 - 1
+
+def annualized_return(results: BacktestResult) -> float:
+    t = (results.records[-1].timestamp - results.records[0].timestamp).days / 365.25
+    vn = results.records[-1].portfolio_value_at_close
+    v1 = results.records[0].portfolio_value_at_close
+    return (vn / v1) ** (1 / t) - 1
+
+def period_returns(results: BacktestResult) -> List[float]:
+    returns = []
+    for prev, next in zip(results.records, results.records[1:]):
+        v_prev = prev.portfolio_value_at_close
+        v_curr = next.portfolio_value_at_close
+        t = v_curr / v_prev - 1
+        returns.append(t)
+
+    return returns
+
+def daily_avg(results: BacktestResult) -> float:
+    rets = period_returns(results)
+    n = len(rets)
+    if n <= 0:
+        return 0
+    return sum(rets) / n
+
+def daily_volatility(results: BacktestResult) -> float:
+    rets = period_returns(results)
+    n = len(rets)
+    if n <= 1:
+        return 0
+    avg = sum(rets) / n
+    variance = sum((r - avg) ** 2 for r in rets) / (n - 1)
+    return sqrt(variance)
+
+def annual_volatility(results: BacktestResult) -> float:
+    return daily_volatility(results) * sqrt(252)
+
+def max_drawdown(results: BacktestResult) -> float:
+    curr_max = float("-inf")
+    drawdowns = []
+    for r in results.records:
+        v = r.portfolio_value_at_close
+        if v > curr_max:
+            curr_max = v
+
+        drawdowns.append(v / curr_max - 1)
+
+    return min(drawdowns)
+
+def daily_sharpe_ratio(results: BacktestResult) -> float:
+    return daily_avg(results) / daily_volatility(results)
+
+def annual_sharpe_ratio(results: BacktestResult) -> float:
+    return daily_sharpe_ratio(results) * sqrt(252)
+
+def number_of_trades(results: BacktestResult) -> float:
+    return len(results.trades)
+
