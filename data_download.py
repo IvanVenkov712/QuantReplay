e3efe3b0b8801244853cf89a905744fb6f468879
IvanVenkov712 <ivan.venkov.developer@gmail.com>
2026-08-20 20:16:02 +0300

    fixed csv formats

--- data_download.py
+++ data_download.py
@@ -6,6 +6,29 @@ data = yf.download(
     end="2026-01-01",
     interval="1d",
     auto_adjust=False,
+    multi_level_index=False,
 )
 
 data.to_csv("data/SPY.csv")
+
+data = yf.download(
+    "AAPL",
+    start="2021-01-01",
+    end="2026-01-01",
+    interval="1d",
+    auto_adjust=False,
+    multi_level_index=False,
+)
+
+data.to_csv("data/AAPL.csv")
+
+data = yf.download(
+    "MSFT",
+    start="2021-01-01",
+    end="2026-01-01",
+    interval="1d",
+    auto_adjust=False,
+    multi_level_index=False,
+)
+
+data.to_csv("data/MSFT.csv")
