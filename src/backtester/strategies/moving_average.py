a216fdba7c16d597ffa11e3a1b66b7895fe4f9ab
IvanVenkov712 <ivan.venkov.developer@gmail.com>
2026-08-27 16:08:11 +0300

    Updated moving_average with on_candle

--- src/backtester/strategies/moving_average.py
+++ src/backtester/strategies/moving_average.py
@@ -1,6 +1,5 @@
 """Simple moving-average crossover strategy."""
-
-from typing import Sequence
+from collections import deque
 
 from backtester.domain.market import Candle
 from backtester.strategies.base import Strategy
@@ -21,26 +20,47 @@ class MovingAverageCrossStrategy(Strategy):
         if short_window >= long_window:
             raise ValueError("short_window must be smaller than long_window")
 
-        self.short_window: int = short_window
-        self.long_window: int = long_window
+        self._short_window_len: int = short_window
+        self._long_window_len: int = long_window
 
-    def generate_signal(self, candles: Sequence[Candle]) -> Signal:
-        """Generate a crossover signal from the latest available closes."""
-        if len(candles) < self.long_window + 1:
+        self._long_sum: float= 0
+        self._short_sum: float = 0
+
+        self._long_window = deque(maxlen=self._long_window_len)
+        self._short_window = deque(maxlen=self._short_window_len)
+
+        self._counter = 0
+
+    def on_candle(self, candle: Candle) -> Signal:
+        old_long_avg = self._long_sum / self._long_window_len
+        old_short_avg = self._short_sum / self._short_window_len
+
+        self._update_state(candle.close)
+
+        if self._counter < self._long_window_len:
+            self._counter += 1
             return Signal.HOLD
 
-        old_long_avg = sum(candle.close for candle in candles[-self.long_window - 1:-1]) / self.long_window
-        old_short_avg = sum(candle.close for candle in candles[-self.short_window - 1:-1]) / self.short_window
-
-        long_avg = sum(candle.close for candle in candles[-self.long_window:]) / self.long_window
-        short_avg = sum(candle.close for candle in candles[-self.short_window:]) / self.short_window
-
-        if old_long_avg <= old_short_avg and long_avg > short_avg:
+        new_long_avg = self._long_sum / self._long_window_len
+        new_short_avg = self._short_sum / self._short_window_len
+        
+        if old_short_avg >= old_long_avg and new_short_avg < new_long_avg:
             return Signal.SELL
 
-        elif old_long_avg >= old_short_avg and long_avg < short_avg:
+        elif old_short_avg <= old_long_avg and new_short_avg > new_long_avg:
             return Signal.BUY
 
         else:
             return Signal.HOLD
 
+    def _update_state(self, price: float):
+        if len(self._long_window) >= self._long_window_len:
+            self._long_sum -= self._long_window.popleft()
+
+        if len(self._short_window) >= self._short_window_len:
+            self._short_sum -= self._short_window.popleft()
+
+        self._long_sum += price
+        self._short_sum += price
+        self._long_window.append(price)
+        self._short_window.append(price)
