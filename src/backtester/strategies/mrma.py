39eb65ba5f4d84bfa15fb05303f3ea9a48253c2e
IvanVenkov712 <ivan.venkov.developer@gmail.com>
2026-08-27 16:39:32 +0300

    Updated mrma

--- src/backtester/strategies/mrma.py
+++ src/backtester/strategies/mrma.py
@@ -1,5 +1,5 @@
 """Moving-average mean-reversion strategy."""
-
+from collections import deque
 from typing import Sequence
 
 from backtester.domain.market import Candle
@@ -17,22 +17,51 @@ class MeanReversionStrategy(Strategy):
         if not 0 <= threshold <= 1:
             raise ValueError("threshold must be between 0 and 1")
 
-        self.__window: int = window
-        self.__threshold: float = threshold
+        self._window_len: int = window
+        self._threshold: float = threshold
+        self._sum: float = 0
+        self._window = deque(maxlen=self._window_len)
 
-    def generate_signal(
-        self, candles: Sequence[Candle]) -> Signal:
-        """Generate a signal from the latest close and rolling close mean."""
-
-        if len(candles) < self.__window:
+    def on_candle(self, candle: Candle) -> Signal:
+        price = candle.close
+        self._update_state(candle.close)
+        if len(self._window) < self._window_len:
             return Signal.HOLD
 
-        average = sum(candle.close for candle in candles[-self.__window:]) / self.__window
-        price = candles[-1].close
+        average = self._sum / self._window_len
 
-        if price < average * self.__threshold:
+        if price < average * self._threshold:
             return Signal.BUY
         elif price >= average:
             return Signal.SELL
         else:
             return Signal.HOLD
+
+    def reset(self):
+        self._sum: float = 0
+        self._window.clear()
+
+    def _update_state(self, price: float):
+        if len(self._window) >= self._window_len:
+            self._sum -= self._window.popleft()
+
+        self._sum += price
+        self._window.append(price)
+
+    # def generate_signal(
+    #     self, candles: Sequence[Candle]) -> Signal:
+    #     """Generate a signal from the latest close and rolling close mean."""
+    #
+    #     if len(candles) < self._window_len:
+    #         return Signal.HOLD
+    #
+    #     average = sum(candle.close for candle in candles[-self._window_len:]) / self._window_len
+    #     price = candles[-1].close
+    #
+    #     if price < average * self._threshold:
+    #         return Signal.BUY
+    #     elif price >= average:
+    #         return Signal.SELL
+    #     else:
+    #         return Signal.HOLD
+    #
