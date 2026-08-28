30dd91706424cd84eece85d609e46c2d37645d57
IvanVenkov712 <ivan.venkov.developer@gmail.com>
2026-08-28 16:51:44 +0300

    added new moving average strategy tests

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -5,7 +5,6 @@ import pytest
 from backtester.domain.market import Candle
 from backtester.domain.trading import Signal
 from backtester.strategies.mrma import MeanReversionStrategy
-from backtester.strategies.moving_average import MovingAverageCrossStrategy
 from backtester.strategies.rsi_strategies import RSIStrategy, calculate_simple_rsi
 
 
@@ -23,70 +22,6 @@ def make_candles(closes: list[float]) -> list[Candle]:
         )
         for index, close in enumerate(closes)
     ]
-
-
-@pytest.mark.parametrize(
-    ("short_window", "long_window"),
-    [
-        (0, 3),
-        (-1, 3),
-        (2, 0),
-        (3, -1),
-    ],
-)
-def test_moving_average_rejects_non_positive_windows(
-    short_window: int,
-    long_window: int,
-) -> None:
-    with pytest.raises(ValueError, match="Window sizes must be positive"):
-        MovingAverageCrossStrategy(short_window=short_window, long_window=long_window)
-
-
-@pytest.mark.parametrize(
-    ("short_window", "long_window"),
-    [
-        (3, 3),
-        (4, 3),
-    ],
-)
-def test_moving_average_requires_short_window_smaller_than_long_window(
-    short_window: int,
-    long_window: int,
-) -> None:
-    with pytest.raises(ValueError, match="short_window must be smaller than long_window"):
-        MovingAverageCrossStrategy(short_window=short_window, long_window=long_window)
-
-
-def test_moving_average_holds_until_enough_candles_for_crossover() -> None:
-    strategy = MovingAverageCrossStrategy(short_window=2, long_window=3)
-
-    signal = strategy.generate_signal(make_candles([10, 11, 12]))
-
-    assert signal is Signal.HOLD
-
-
-def test_moving_average_buys_when_short_average_crosses_above_long_average() -> None:
-    strategy = MovingAverageCrossStrategy(short_window=2, long_window=3)
-
-    signal = strategy.generate_signal(make_candles([10, 10, 8, 14]))
-
-    assert signal is Signal.BUY
-
-
-def test_moving_average_sells_when_short_average_crosses_below_long_average() -> None:
-    strategy = MovingAverageCrossStrategy(short_window=2, long_window=3)
-
-    signal = strategy.generate_signal(make_candles([10, 10, 12, 6]))
-
-    assert signal is Signal.SELL
-
-
-def test_moving_average_holds_when_averages_do_not_cross() -> None:
-    strategy = MovingAverageCrossStrategy(short_window=2, long_window=3)
-
-    signal = strategy.generate_signal(make_candles([10, 11, 12, 13]))
-
-    assert signal is Signal.HOLD
 
 
 @pytest.mark.parametrize("window", [0, -1])
